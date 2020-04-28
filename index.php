9ee0b48115124be5d3e3863225c6dabba864da33
Michael Gulchuk <mgulchuk@mail.greenriver.edu>
2020-04-27 22:47:39 -0400

    set up defualt route

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $f3 = Base::instance();
 // Define a default route
 $f3->route('GET /', function()
 {
-    //echo '<h1>Welcome food</h1>';
+    //echo '<h1>Welcome to my dating website</h1>';
     $view = new Template();
     echo $view -> render("views/home.html");
 });
